285cef101b75bce974dc5b07ec1063cf9ec95e4b
aperalta <aperalta@veritran.com>
2021-02-06 12:27:44 -0500

    Se agregó /about #9

---
+++ logic.py
@@ -0,0 +1,6 @@
+def get_about_this(VERSION):
+    response = (
+        f"Simple Expenses Bot (pyTelegramBot) v{VERSION}"
+        "\n\n""Desarrollado por Alejandra Peralta <alejandra.peraltad@autonoma.edu.co>")
+        
+    return response
