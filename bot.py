ff4ff251a33754339532d1109db3103855c3fee9
aperalta <aperalta@veritran.com>
2021-02-06 12:17:06 -0500

    Se implementa persistencia de datos

--- bot.py
+++ bot.py
@@ -1,116 +1,72 @@
+#########################################################
+
 from config import bot
 import config
 from time import sleep
-import re                   
+import re
+import logic
+import database.db as db
+
+#########################################################
+
+if __name__ == '__main__':
+	db.Base.metadata.create_all(db.engine)
+
+#########################################################
 
 @bot.message_handler(commands=['start'])
 def on_command_start(message):
-	bot.send_chat_action(message.chat.id, 'typing')
-	sleep(1)
-	
-	bot.send_message(
-		message.chat.id,
-		"Hola, soy un \U0001F916, ¿cómo estás?", 
-		parse_mode="Markdown")
+	pass
+
+#########################################################
 
 @bot.message_handler(commands=['help'])
 def on_command_help(message):
-	bot.send_chat_action(message.chat.id, 'typing')
-	sleep(1)
-	
-	response = (
-				"Estos son los comandos y órdenes disponibles:\n"
-				"\n"
-				"*/start* - Inicia la interacción con el bot\n"
-				"*/help* - Muestra este mensaje de ayuda\n"
-				"*sumar {valor1} y {valor2}* - Calcula la suma de dos valores\n"
-				"*restar {valor1} y {valor2}* - Calcula la resta de dos valores\n"
-				"*multiplicar {valor1} y {valor2}* - Calcula la multiplicación de dos valores\n"
-				"*dividir {valor1} y {valor2}* - Calcula la división de dos valores\n"
-				)
+	pass
 
-	bot.send_message(
-		message.chat.id,
-		response,
-		parse_mode="Markdown")
+#########################################################
 
-@bot.message_handler(regexp=r"^sumar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
-def on_add(message):
-	bot.send_chat_action(message.chat.id, 'typing')
-	sleep(1)
-	
-	parts = re.match(
-		r"^sumar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$", message.text)
-		
-	# print (parts.groups())
-	oper1 = float(parts[1])
-	oper2 = float(parts[3])
-		
-	result = oper1 + oper2
-		
-	bot.reply_to(
-		message,
-		f"\U0001F522 Resultado: {result}")
+@bot.message_handler(commands=['about'])
+def on_command_about(message):
+	pass
 
-@bot.message_handler(regexp=r"^restar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
-def on_add(message):
-	bot.send_chat_action(message.chat.id, 'typing')
-	sleep(1)
-	
-	parts = re.match(
-		r"^restar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$", message.text)
-		
-	# print (parts.groups())
-	oper1 = float(parts[1])
-	oper2 = float(parts[3])
-		
-	result = oper1 - oper2
-		
-	bot.reply_to(
-		message,
-		f"\U0001F522 Resultado: {result}")
+#########################################################
 
-@bot.message_handler(regexp=r"^multiplicar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
-def on_add(message):
-	bot.send_chat_action(message.chat.id, 'typing')
-	sleep(1)
-	
-	parts = re.match(
-		r"^multiplicar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$", message.text)
-		
-	# print (parts.groups())
-	oper1 = float(parts[1])
-	oper2 = float(parts[3])
-		
-	result = oper1 * oper2
-		
-	bot.reply_to(
-		message,
-		f"\U0001F522 Resultado: {result}")
+@bot.message_handler(regexp=r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_earn_money(message):
+	pass
 
-@bot.message_handler(regexp=r"^dividir ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
-def on_add(message):
-	bot.send_chat_action(message.chat.id, 'typing')
-	sleep(1)
-	
-	parts = re.match(
-		r"^dividir ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$", message.text)
-		
-	# print (parts.groups())
-	oper1 = float(parts[1])
-	oper2 = float(parts[3])
-	
-	if oper2 == 0:
-		bot.send_message(
-		message.chat.id,
-		"\U0001F63F Ups, no se puede divir por 0")
-		
-	else:
-		result = oper1 / oper2
-		
-		bot.reply_to(
-			message,
-			f"\U0001F522 Resultado: {result}")
+#########################################################
+
+@bot.message_handler(regexp=r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_spend_money(message):
+	pass
+
+#########################################################
+
+@bot.message_handler(regexp=r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_earnings(message):
+	pass
+
+#########################################################
+
+@bot.message_handler(regexp=r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_spendings(message):
+	pass
+
+#########################################################
+
+@bot.message_handler(regexp=r"^(obtener saldo|s)$")
+def on_get_balance(message):
+	pass
+
+#########################################################
+
+@bot.message_handler(regexp=r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$")
+def on_remove_record(message):
+	pass
+
+#########################################################
 
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
@@ -120,6 +76,8 @@ def on_fallback(message):
 	bot.reply_to(
 		message,
 		"\U0001F63F Ups, no entendí lo que me dijiste.")
+
+#########################################################
 
 if __name__ == '__main__':
 	  bot.polling(timeout=20)
